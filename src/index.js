3eb56ccb7b7a15f7920ee38a26bca9d7407605aa
SarvT <91484197+SarvT@users.noreply.github.com>
2023-07-01 21:13:02 +0530

    url mods

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-handle";
+import Homepage from "./components/Homepage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Homepage />,
     errorElement: <ErrorPage/>
   },
   // {
